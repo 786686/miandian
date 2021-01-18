a2d5093c9a44973226657adce9ee80161bec140f
786686 <786686227@qq.com>
2021-01-19 00:15:17 +0800

    首页

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -15,12 +15,12 @@
         <div class="item-l">
           <div class="item-cover">
             <van-image
-              :src="require('@/assets/image/ic_xiala.png')"
+              :src="require('@/assets/image/pic.jpg')"
             ></van-image>
           </div>
           <div class="item-l-bottom">
-            <van-image :src="require('@/assets/image/ic_xiala.png')"></van-image
-            >5
+            <van-image :src="require('@/assets/image/pic_number.png')"></van-image
+            ><div class="item-num">5</div>
           </div>
         </div>
         <div class="item-r">
@@ -28,19 +28,19 @@
             <div class="item-name">姓名</div>
             <div class="item-score">
               <van-image
-                :src="require('@/assets/image/ic_xiala.png')"
+                :src="require('@/assets/image/ic_star.png')"
               ></van-image
               ><van-image
-                :src="require('@/assets/image/ic_xiala.png')"
+                :src="require('@/assets/image/ic_star.png')"
               ></van-image
               ><van-image
-                :src="require('@/assets/image/ic_xiala.png')"
+                :src="require('@/assets/image/ic_star.png')"
               ></van-image
               ><van-image
-                :src="require('@/assets/image/ic_xiala.png')"
+                :src="require('@/assets/image/ic_star.png')"
               ></van-image
               ><van-image
-                :src="require('@/assets/image/ic_xiala.png')"
+                :src="require('@/assets/image/ic_star.png')"
               ></van-image>
               4分
             </div>
@@ -77,72 +77,116 @@ export default {
   width: 100;
   border-bottom: 1px solid #eee;
   display: flex;
+  height: 48px;
   .item {
+    
+    font-size: 15px;
     flex: 1;
     display: flex;
     align-items: center;
+    justify-content: center;
+    color: #666;
     .van-image {
+      width: 14px; height: 14px; margin-left: 2px;
     }
     &.active {
-      color: #333;
+      color: #D627FA;
+      font-weight: bold;
     }
   }
 }
 .index-list {
-  margin-left: 10px;
+  margin-left: 16px;
   .item {
     display: flex;
     padding: 10px 0;
     .item-l {
-      .item-cover {
-        width: 40px;
-        height: 40px;
-        border-radius: 10px;
-        margin-right: 10px;
+      overflow: hidden;
+      width: 110px;
+      height: 110px;
+      border-radius: 6px;
+        margin-right: 12px; 
+      .item-cover .van-image {
+        width: 110px;
+        height: 110px; 
+        border-radius: 6px;
       }
       position: relative;
       .item-l-bottom {
+        position: absolute;
+        right: 0; bottom: 0;
         color: #fff;
-        font-size: 14px;
+        font-size: 13px;
+        z-index: 2;
+        text-align: right;
+        line-height: 24px;
+        padding-right: 4px;
+        .item-num{position: relative; z-index: 2;}
         .van-image {
-          width: 10px;
-          height: 10px;
+          z-index: 1;
+          position: absolute;
+          right: 0; bottom: 0;
+          width: 36px;
+          height: 24px;
         }
       }
     }
     .item-r {
+      padding-right: 16px;
       flex: 1;
       .item-r-top {
         display: flex;
         justify-content: space-between;
+        align-items: center;
+        margin-bottom: 10px;
         .item-name {
+          font-size: 16px;
+          color: #333333;
+          line-height: 22px;
         }
-        .item-sorce {
-          font-size: 14px;
-          color: #999;
+        .item-score {
+          
+        display: flex;
+        align-items: center;
+font-size: 13px;
+color: #999999;
+line-height: 18px;
           .van-image {
-            width: 10px;
-            height: 10px;
-            margin-right: 5px;
+            width: 16px;
+            height: 16px;
+            margin-right: 4px;
+            &:last-child{
+              margin-right: 8px;
+            }
           }
         }
       }
       .item-tags {
         display: flex;
+        flex-wrap: nowrap;
         .item-tag {
-          margin-right: 5px;
-          background-color: #f4f4f4;
-          color: #f4f4f4;
+          margin-right: 4px;
+          font-size: 12px;
+          color: #B3B5BA;
+          height: 22px;
+          line-height: 22px;
+          background: #F8F9FB;
           border-radius: 2px;
+          padding: 0 5px;
         }
       }
       .item-price {
-        font-size: 24px;
-        color: #f00;
+        font-size: 17px;
+        font-weight: bold;
+        color: #F53838;
+        line-height: 24px;
+        margin: 6px 0;
       }
       .item-intro {
-        color: #999;
-        font-size: 12px;
+        
+        font-size: 13px;
+        color: #999999;
+        line-height: 16px;
         margin-bottom: 10px;
       }
       border-bottom: 1px solid #eee;
--- src/views/SetPwd/SetPwd.vue
+++ src/views/SetPwd/SetPwd.vue
@@ -1,37 +1,27 @@
-<style scoped lang="scss">
-  .set-pwd{
-    margin: 40px 70px;
-    
-font-size: 16px;
-font-weight: bold;
-color: #525050;
-line-height: 22px;
-text-align: center;
-    .item-link{
-color: #2B7FD2;
-    }
-  }
-.set-tips{
-  margin: 10px 30px; 
-  
-font-size: 13px;
-color: #999999;
-line-height: 18px;
-}
-</style>
-
 <template>
   <div>
-    <div class="set-pwd">
-      您已设置了支付密码，<br>
-      如您忘记密码，请<span class="item-link">联系客服</span>找回
+    <div class="set-dec">请输入新支付密码，用于提现</div>
+    <div class="pwd-inp">
+      <div class="item" @click="setFocus" v-for="(item,index) in 6" :key="index"><div class="item-dot" v-if="pwdVal.length>index"></div></div>
     </div>
+    
     <div class="btns">
       <div class="btn">修改密码</div>
     </div>
     <div class="set-tips">
       说明：支付密码用于支付及提现，非常重要，请务必记住，如您忘记了支付密码，请联系客服找回。
     </div>
+    
+    <input
+      ref="passwordref"
+      v-model="pwdVal"
+      :focus="focus"
+      maxlength="6"
+      type="number"
+      class="hidden-input"
+      @input="inputVal"
+      @blur="setBlur"
+    >
   </div>
 </template>
 <script>
@@ -40,6 +30,11 @@ export default {
   name: "wallet",
   data() {
     return {
+      sum: 6,
+      pwdVal:"",
+      focus: false,
+      isAndroid: /android/.test(window.navigator.userAgent.toLocaleLowerCase())
+
     };
   },
   computed: {
@@ -47,12 +42,72 @@ export default {
   created() {
   },
   methods: {
-    
+    getFocus(){
+      this.focus = true;
+    },
+    setBlur() {
+      this.focus = false
+    },
+    setFocus() {
+      this.focus = true
+      if (this.isAndroid) {
+        window.addEventListener('resize', function() {
+          if (document.activeElement.tagName === 'input' || document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'textarea' || document.activeElement.tagName === 'TEXTAREA') {
+            window.setTimeout(function() {
+              document.activeElement.scrollIntoViewIfNeeded()
+            }, 0)
+          }
+        })
+      }
+      this.$refs.passwordref.focus()
+    },
+    inputVal() {
+      if (this.pwdVal.length > 6) {
+        this.pwdVal = this.pwdVal.substr(0, 6)
+      }
+      setTimeout(() => {
+        this.pwdVal = this.pwdVal.replace('.', '')
+      }, 0)
+      
+    }
   },
   mounted() {
   }
 };
 </script>
-<style lang="scss" scoped>
+<style scoped lang="scss">
+  .set-dec{
+    margin: 34px 0 30px;
+    text-align: center;
+    font-size: 13px;
+    color: #999999;
+    line-height: 18px;
+  }
+  .pwd-inp{
+    display: flex;
+    margin: 0 15px 40px;
+    justify-content: space-between;
+    .item{
+      width: 49px; height: 49px;
+      border: 1px solid #EAEAEA;
+      display: flex; justify-content: center; align-items: center;
+      .item-dot{
+        width: 15px; height: 15px; border-radius: 50%;
+        background-color: #666;
+      }
+    }
 
+  }
+
+  .set-tips{
+    margin: 10px 30px; 
+    font-size: 13px;
+    color: #999999;
+    line-height: 18px;
+  }
+  
+/* 文本输入框位置: 设置到左边隐藏 */
+.hidden-input { 
+  position: relative; left: -300px; bottom: 0; width: 100px; height: 100px;
+}
 </style>
